86eecf952cd8ad33a263b09a74b04c0223ee128a
talexelias <alex.elias@pumexinfotech.com>
2025-04-09 09:31:39 +0530

    Resolves #29 The 3D animation screen suddenly turns white during interaction or loading.

--- src/components/Landing/LandingSection1/landingSection.tsx
+++ src/components/Landing/LandingSection1/landingSection.tsx
@@ -1,8 +1,43 @@
 "use client";
+
+import { useEffect, useMemo, useState } from "react";
 import "./landingSection.css";
 import Spline from "@splinetool/react-spline";
 
 const LandingSection: React.FC = () => {
+  const [isWebGLAvailable, setIsWebGLAvailable] = useState(true);
+
+  useEffect(() => {
+    const canvas = document.querySelector("canvas");
+
+    const handleContextLost = (e: Event) => {
+      e.preventDefault();
+      console.warn("WebGL context lost. Displaying fallback UI.");
+      setIsWebGLAvailable(false);
+    };
+
+    canvas?.addEventListener("webglcontextlost", handleContextLost, false);
+
+    return () => {
+      canvas?.removeEventListener("webglcontextlost", handleContextLost);
+    };
+  }, []);
+
+  const splineCanvas = useMemo(() => {
+    return isWebGLAvailable ? (
+      <Spline
+        className="spline-render"
+        scene="https://d2l4gl47o0xxs9.cloudfront.net/splinecube.spline"
+      />
+    ) : (
+      <div className="spline-fallback">
+        <p>
+          3Dコンテンツが利用できません。更新するか、対応するブラウザをお試しください。
+        </p>
+      </div>
+    );
+  }, [isWebGLAvailable]);
+
   return (
     <div
       className="landing-section-one-main-div"
@@ -11,10 +46,7 @@ const LandingSection: React.FC = () => {
       }}
     >
       <div className="landing-section-one-main-div-top">
-        <Spline
-          className="spline-render"
-          scene="https://d2l4gl47o0xxs9.cloudfront.net/splinecube.spline"
-        />
+        {splineCanvas}
         <div className="overlay"></div>
       </div>
       <div className="landing-section-one-main-div-bottom">
